8c3d981571ba97863bba30c1159d68f6519cac90
lyq1996 <lyq123233@gmail.com>
2020-08-04 14:02:59 +0800

    update

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,7 +5,7 @@ int main(){
     std::vector<int> output(10,0);
     std::vector<int>::iterator itr = output.begin();
     for(;itr!=output.end();itr++){
-        std::cout << "hello CI" << std::endl;
+        std::cout << "yes you are good" << std::endl;
     }
     return 0;
 }
