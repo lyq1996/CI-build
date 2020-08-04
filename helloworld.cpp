9fddca45771358293690bcee284044bb5f28164f
lyq1996 <lyq123233@163.com>
2020-08-04 20:10:16 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,7 +5,7 @@ int main(){
     std::vector<int> output(10,0);
     std::vector<int>::iterator itr = output.begin();
     for(;itr!=output.end();itr++){
-        std::cout << "The new branch" << std::endl;
+        std::cout << "Tshe new branch" << std::endl;
     }
     return 0;
 }
